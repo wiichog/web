69531fe0146f7c6ab22240b0bd0e32aca269034c
Juan Luis Garcia <gar14189@uvg.edu.gt>
2017-08-06 21:29:29 -0600

    sending messages to server with POST

--- CHAT/myjs.js
+++ CHAT/myjs.js
@@ -10,15 +10,24 @@ wage.addEventListener("keydown", function (e) {
 });
 
 function validate(e) {
+    httpPOST()
     httpGet();
 }
 
-function httpGet()
-{
+function httpGet(){
   $.get("http://52.88.26.79:7000/", function(responseText) {
     var messages = $.parseJSON( responseText )
     for(var i=0;i<messages.length;i++){
       chat.innerHTML += '<div class="messages"> <p style="font-weight: bold;">'+ messages[i].username +':</p> ' + messages[i].chatmessage + '</div>'
     }
 });
+}
+
+function httpPOST(){
+  $.post("http://52.88.26.79:7000/",{
+    "username":"wiichog",
+    "chatmessage":document.getElementById('message').value
+  })
+  chat.innerHTML = "";
+  document.getElementById('message').value="";
 }
